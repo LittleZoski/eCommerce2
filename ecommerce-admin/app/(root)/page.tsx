fd60230927daf315b30b5d9c8c184afac9c8a567
Frank <shicheng.zhou@my.utsa.edu>
2023-10-23 22:39:25 -0500

    ec

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div className="p-6"><Button size="lg" variant="ghost">Click me</Button></div>
+     
+    )
+  }
+  
